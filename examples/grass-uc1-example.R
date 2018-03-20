463e4a84b3e8d66fb0334d9aef9e7095f756c90e
Florian Lahn <florian.lahn@uni-muenster.de>
2018-03-20 14:09:29 +0100

    grass gis back-end examples

---
+++ examples/grass-uc1-example.R
@@ -0,0 +1,53 @@
+library(devtools)
+install_github(repo = "Open-EO/openeo-r-client", ref = "0.1.0")
+
+
+library(openeo)
+library(httr)
+library(raster)
+
+graasHost = "http://openeo.mundialis.de:5000"
+
+con = connect(host=graasHost, disable_auth = TRUE)
+con %>% capabilities()
+
+collection_names = (con %>% listCollections())$product_id
+con %>% listProcesses()
+
+str(con %>% describeCollection("LL.sentinel2A_openeo_subset.strds.S2A_B04"))
+
+str(con %>% describeProcess("NDVI"))
+
+s2a_b04 = collection("LL.sentinel2A_openeo_subset.strds.S2A_B04",id_name = "product_id")
+filter_bbox_1 = process(process_id = "filter_bbox",collections=list(s2a_b04), bottom=38.9, left=-4.8,right=-4.6,top=39.1,ewres=0.0001, nsres=0.0001, srs="EPSG:4326")
+ndvi__b4_chain = process(process_id="filter_daterange", collections=list(filter_bbox_1), from="2017-04-12 11:17:08", to = "2017-09-04 11:18:26")
+
+s2a_b08 = collection("LL.sentinel2A_openeo_subset.strds.S2A_B08",id_name = "product_id")
+filter_bbox_2 = process(process_id="filter_bbox",collections=list(s2a_b08), bottom=38.9, left=-4.8,right=-4.6,top=39.1,ewres=0.0001, nsres=0.0001, srs="EPSG:4326")
+ndvi__b8_chain = process(process_id="filter_daterange", collections=list(filter_bbox_2), from="2017-04-12 11:17:08", to = "2017-09-04 11:18:26")
+
+ndvi_calc = process(process_id="NDVI",collections=list(ndvi__b4_chain,ndvi__b8_chain),red="S2A_B04",nir="S2A_B08")
+task = process(process_id="min_time",collections=list(ndvi_calc))
+exportedTask = process(process_id = "raster_exporter", collections=list(task))
+
+job_id = con %>% queueTask(list(process_graph=exportedTask)) #for API 0.0.1 it should be only the process graph and not wrapped
+# note that queueTask is the equivalent to ...?evaluate=lazy and it is directly processed
+
+job = con %>% queryJob(job_id)
+job$resources[[1]]
+
+# con %>% openeo:::deleteJob(job_id)
+
+
+img = GET(job$resources[[1]])
+writeBin(content(img),"graas-uc1.tiff")
+r = raster("graas-uc1.tiff")
+spplot(r)
+
+r2 = con$uploadJob(task = list(process_graph = exportedTask),"batch") #batch mode
+job2 = con %>% queryJob(r2)
+job2$status
+job2$resources[[1]]
+
+# con %>% openeo:::deleteJob(r2)
+
---
+++ examples/grass-uc3-example.R
@@ -0,0 +1,24 @@
+library(devtools)
+install_github(repo = "Open-EO/openeo-r-client", ref = "0.1.0")
+
+
+library(openeo)
+library(httr)
+library(raster)
+
+product = "LL.openeo_mapset_0.strds.S2A_B08_filter_daterange_NDVI"
+regions_url = "https://storage.googleapis.com/graas-geodata/roi_openeo_use_case_2.geojson"
+
+coll = collection(product,id_name="product_id")
+box = process(process_id = "filter_bbox", collections=list(coll), bottom=38.9, left=-4.8, right= -4.6, top = 39.1, ewres=0.0001,nsres=0.0001,srs="EPSG:4326")
+dr = process(process_id="filter_daterange", collections=list(box),from="2017-04-12 11:17:08",to="2017-09-04 11:18:26")
+zonal = process(process_id="zonal_statistics",collections=list(dr), regions=regions_url)
+
+job_id = con$uploadJob(task = list(process_graph = zonal),"batch") #batch mode
+
+job = con %>% queryJob(job_id)
+job$status
+
+info = job$job_info
+
+
