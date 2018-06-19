1a0cc6186d73ae52e92bf89f25ca7e8a9894edd1
Florian Lahn <florian.lahn@uni-muenster.de>
2018-06-19 21:32:22 +0200

    modified process_graph creation defaults
    * product now uses "product_id" as the default name in collections of the process graph
    * processes now use "imagery" as a default for the parameter that contains the processes / or image data

--- R/process_graph_building.R
+++ R/process_graph_building.R
@@ -3,10 +3,10 @@
 #' defines a process with arguments
 #' @param process A process that might be chained with this process
 #' @param process_id ID of the process offered by the connected openEO backend
-#' @param prior.name The name of the prior process / collection, default "collections"
+#' @param prior.name The name of the prior process / collection, default "imagery"
 #' @param ... named arguments that are passed to the process description
 #' @export
-process = function(process=NULL, process_id, prior.name="collections", ...) {
+process = function(process=NULL, process_id, prior.name="imagery", ...) {
   # type check "process" either collection or process
   res = list()
   arguments = list()
@@ -35,12 +35,12 @@ process = function(process=NULL, process_id, prior.name="collections", ...) {
 #' A collection object
 #'
 #' creates a list representation of a collection object
-#' @param collection_id the id of the product
-#' @param id_name the name of the identifier for this collection, default "collection_id"
+#' @param product_id the id of the product
+#' @param id_name the name of the identifier for this collection, default "product_id"
 #' @return a list represenation for a collection / product
 #' @export
-collection = function(collection_id,id_name = "collection_id") {
-  result = list(collection_id)
+collection = function(product_id,id_name = "product_id") {
+  result = list(product_id)
   names(result) = id_name
   
   attr(result, "type") <- "collection"
