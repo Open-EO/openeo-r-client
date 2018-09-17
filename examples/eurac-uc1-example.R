f89597e312b004e256a5eda5f4fb8cde8af94f25
Florian Lahn <florian.lahn@uni-muenster.de>
2018-09-17 17:58:40 +0200

    added dplyr as import, adapted EURAC UC1 for pgb()

--- examples/eurac-uc1-example.R
+++ examples/eurac-uc1-example.R
@@ -4,16 +4,17 @@ euracHost = ""
 
 eurac = connect(host = euracHost,disable_auth = TRUE)
 # eurac %>% listProcesses()
-eurac %>% capabilities()
-str(eurac %>% formats())
+eurac %>% listCapabilities()
+str(eurac %>% listFormats())
 
 eurac %>% listCollections()
+eurac %>% describeCollection("S2_L2A_T32TPS_20M")
 
-str(eurac %>% describeCollection("S2_L2A_T32TPS_20M"))
-str(eurac %>% describeProcess("filter_bbox"))
-str(eurac %>% describeProcess("filter_daterange"))
-str(eurac %>% describeProcess("NDVI"))
-str(eurac %>% describeProcess("min_time"))
+eurac %>% describeCollection("S2_L2A_T32TPS_20M")
+eurac %>% describeProcess("filter_bbox")
+eurac %>% describeProcess("filter_daterange")
+eurac %>% describeProcess("NDVI")
+eurac %>% describeProcess("min_time")
 
 task = collection("S2_L2A_T32TPS_20M") %>% 
   process("filter_bbox", prior.name = "imagery", left=652000,right=672000,bottom=5181000,top=5161000,srs="EPSG:32632") %>% 
@@ -21,7 +22,17 @@ task = collection("S2_L2A_T32TPS_20M") %>%
   process("NDVI", prior.name = "imagery", red="B04",nir="B8A") %>% 
   process("min_time",prior.name="imagery")
 
+# Build a process graph using the ProcesGraphBuilder pgb()
+pgb = eurac %>% pgb()
+
+alternative = collection("S2_L2A_T32TPS_20M") %>%
+  pgb$filter_bbox(left = 652000, right = 672000, bottom = 5181000, top=5161000,srs="EPSG:32632") %>%
+  pgb$filter_daterange(from = "2017-01-01",to = "2017-01-31") %>%
+  pgb$NDVI(red = "B04", nir = "B8A") %>% 
+  pgb$min_time()
+
 taskToJSON(task)
+taskToJSON(alternative)
 
 raster = eurac %>% executeTask(task=task,
                                format="tiff",
