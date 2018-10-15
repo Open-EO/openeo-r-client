e4e8b4a4e220790817de97382240c3f3a5a2791d
Florian Lahn <florian.lahn@uni-muenster.de>
2018-10-15 14:39:37 +0200

    used httrs oauth2.0 functionality and cleaned no longer needed code

--- R/oidc-client.R
+++ R/oidc-client.R
@@ -12,16 +12,96 @@ OIDCClient = R6Class(
     # functions ====
     initialize = function(host=NULL){
       if (!is.null(host)) {
-        self$setHost(host)
-        self$getEndpoints()
+        private$setHost(host)
+        private$getEndpoints()
       }
     },
     
-    setHost = function(host) {
-      if (!endsWith(host,"/")) {
-        private$host = paste(host,"/",sep="")
-      }
+    login = function() {
+      openeo_endpoints = structure(list(
+        authorize = private$endpoints$authorization_endpoint,
+        access = private$endpoints$token_endpoint
+      ),class="oauth_endpoint")
+      
+      app = oauth_app("openeo_login",private$client_id)
+      
+      suppressWarnings(
+        private$auth <- oauth2.0_token(openeo_endpoints, app,cache=FALSE,scope="openid")
+      )
       invisible(self)
+    },
+
+    logout = function() {
+      if (is.null(private$auth)) {
+        message("Not logged in.")
+        return(NULL)
+      }
+      url = parse_url(private$endpoints$end_session_endpoint)
+      response = GET(url, query = list(id_token_hint = private$auth$credentials$id_token))
+      if (response$status < 400) {
+        message("Successfully logged out.")
+        private$auth = NULL
+        invisible(TRUE)
+      } else {
+        return(response)
+      }
+    },
+    
+    getUserData = function() {
+      url = parse_url(private$endpoints$userinfo_endpoint)
+      response = GET(url, add_headers(Authorization = paste("Bearer",private$auth$credentials$access_token)))
+      
+      if (response$status < 400) {
+        return(content(response,as="parsed",type="application/json"))
+      } else {
+        return(response)
+      }
+    }
+  ),
+  # active ----
+  active = list(
+    authentication = function() {
+      return(private$auth)
+    },
+    
+    access_token = function() {
+      if (!is.null(private$auth)) {
+        if (private$isExpired(private$auth$credentials$access_token)) {
+          if (!private$isExpired(private$auth$credentials$refresh_token)) {
+            private$auth$refresh()
+          } else {
+            message("Cannot provide access_token. You have to login.")
+            return(invisible(NULL))
+          }
+        }
+        return(private$auth$credentials$access_token)
+      } else {
+        message("Please login first, in order to obtain an access token")
+        return(invisible(NULL))
+      }
+    }
+  ),
+  # private ----
+  private=list(
+    # attributes ====
+    host = NA,
+    # client_id = "openeo-r-client", # predefined in OpenID Provider configuration (should be cryptic :))
+    # redirect_uri = "http://localhost:8764/auth/oidc/cb", # also registered
+    client_id = "openeo-r-client_2",
+    endpoints = list(),
+    auth = NULL, # httr oauth2.0 token object
+    
+    # functions ====
+    
+    isExpired = function(token) {
+      expiry = as_datetime(private$decodeToken(token,2)$exp, tz=Sys.timezone())
+      
+      return(expiry <= Sys.time())
+    },
+    
+    decodeToken = function(access_token, token_part) {
+      tokens = unlist(strsplit(access_token,"\\."))
+      fromJSON(rawToChar(base64enc::base64decode(tokens[token_part])))
     },
     
     getEndpoints = function() {
@@ -38,186 +118,11 @@ OIDCClient = R6Class(
       invisible(self)
     },
     
-    login = function() {
-      openeo_endpoints = structure(list(
-        authorize = private$endpoints$authorization_endpoint,
-        access = private$endpoints$token_endpoint
-      ),class="oauth_endpoint")
-      app = oauth_app("openeo_login",private$client_id)
-      
-      suppressWarnings(
-        mytoken <- oauth2.0_token(openeo_endpoints, app,cache=FALSE,scope="openid")
-      )
-      
-      
-      # browser()
-      
-      # url = parse_url(private$endpoints$authorization_endpoint)
-      # url$query = self$authCodeQueryConfig
-      
-      # open a browser to insert credentials and authorize the client, then copy the redirect link and insert
-      # it into the console
-      # browseURL(build_url(url))
-      # redirectionURL = readline("Please insert redirection URL:\n")
-      # results = private$parseRedirectURI(redirectionURL)
-      
-      # private$authCode = results$code
-      # private$session_state = results$session_state
-      
-      # self$authorize()
-      private$setTokenInformation(mytoken$credentials)
-      invisible(self)
-    },
-    getAccessToken = function() {
-      if (private$isExpired(private$access_token)) {
-        if (!private$isExpired(private$refresh_token)) {
-          self$refreshAccessToken()
-        } else {
-          message("Cannot provide access_token. You have to login.")
-          return(invisible(NULL))
-        }
-      }
-      return(private$access_token)
-    },
-    
-    authorize = function() {
-      url = parse_url(private$endpoints$token_endpoint)
-      
-      response = POST(url, body = self$accessTokenBodyConfig, encode = "form")
-      
-      if (response$status < 400) {
-        results = content(response,as="parsed",type="application/json")
-        private$setTokenInformation(results)
-        
-      } else {
-        message("ERROR: Cannot obtain access token. Not logged in or token expired.")
+    setHost = function(host) {
+      if (!endsWith(host,"/")) {
+        private$host = paste(host,"/",sep="")
       }
       invisible(self)
-    },
-    refreshAccessToken = function() {
-      url = parse_url(private$endpoints$token_endpoint)
-      
-      response = POST(url, body = self$refreshTokenBodyConfig, encode = "form")
-      
-      if (response$status < 400) {
-        results = content(response,as="parsed",type="application/json")
-        private$setTokenInformation(results)
-      } else {
-        message("ERROR: Cannot refresh access token. Not logged in or token expired.")
-      }
-      invisible(self)
-    },
-    logout = function() {
-      url = parse_url(private$endpoints$end_session_endpoint)
-      response = GET(url, query = list(id_token_hint = private$id_token))
-      if (response$status < 400) {
-        message("Successfully logged out.")
-        invisible(TRUE)
-      } else {
-        return(response)
-      }
-    },
-    getUserData = function() {
-      url = parse_url(private$endpoints$userinfo_endpoint)
-      response = GET(url, add_headers(Authorization = paste("Bearer",private$access_token)))
-      
-      if (response$status < 400) {
-        return(content(response,as="parsed",type="application/json"))
-      } else {
-        return(response)
-      }
-    }
-  ),
-  # active ----
-  active = list(
-    authCodeQueryConfig = function() {
-      return(
-        list(
-          client_id = private$client_id,
-          redirect_uri = private$redirect_uri,
-          response_type = "code",
-          scope = "openid"
-        )
-      ) 
-    },
-    
-    accessTokenBodyConfig = function() {
-      return(list(
-        grant_type="authorization_code",
-        client_id = private$client_id,
-        redirect_uri = private$redirect_uri,
-        code = private$authCode
-      ))
-    },
-    
-    refreshTokenBodyConfig = function() {
-      return(
-        list(
-          grant_type = "refresh_token",
-          refresh_token = private$refresh_token,
-          client_id = private$client_id,
-          redirect_uri = private$redirect_uri,
-          scope="openid"
-        )
-      )
-    }
-  ),
-  # private ----
-  private=list(
-    # attributes ====
-    host = NA,
-    # client_id = "openeo-r-client", # predefined in OpenID Provider configuration (should be cryptic :))
-    # redirect_uri = "http://localhost:8764/auth/oidc/cb", # also registered
-    client_id = "openeo-r-client_2",
-    redirect_uri = "http://localhost:1410", # also registered
-    endpoints = list(),
-    authCode = NA,
-    session_state = NA,
-    access_token = NA,
-    refresh_token = NA,
-    id_token = NA,
-    # functions ====
-    
-    setTokenInformation = function(token_response) {
-      private$access_token = token_response$access_token
-      private$refresh_token = token_response$refresh_token
-      private$id_token = token_response$id_token
-      private$session_state = token_response$session_state
-      
-    },
-    
-    isExpired = function(token) {
-      expiry = as_datetime(private$decodeToken(token,2)$exp, tz=Sys.timezone())
-      
-      return(expiry <= Sys.time())
-    },
-    
-    # parses the redirect URI in a list of KVP based on the query parameters
-    parseRedirectURI = function(redirectURL) {
-      params = unlist(strsplit(unlist(strsplit(redirectURL,"\\?"))[2],"&"))
-      
-      # split each kvp on = and created a named list
-      names = sapply(params, function(kvp) {
-        split = unlist(strsplit(kvp,"="))
-        return(split[1])
-      })
-      values = lapply(params, function(kvp) {
-        split = unlist(strsplit(kvp,"="))
-        return(split[2])
-      })
-      
-      names(values) = names
-      
-      return(values)
-    },
-    
-    decodeToken = function(access_token, token_part) {
-      tokens = unlist(strsplit(access_token,"\\."))
-      fromJSON(rawToChar(base64enc::base64decode(tokens[token_part])))
     }
   )
 )
-
-
-
-
