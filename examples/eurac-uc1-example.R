746f996c774632f78e1daa86a55b31b54fa9024b
Florian Lahn <florian.lahn@uni-muenster.de>
2019-02-07 10:39:34 +0100

    modified UC1 EURAC test case

--- examples/eurac-uc1-example.R
+++ examples/eurac-uc1-example.R
@@ -1,46 +1,40 @@
 library(openeo)
 
-euracHost = ""
+euracHost = "http://saocompute.eurac.edu/openEO_0_3_0/openeo/"
 
 eurac = connect(host = euracHost,disable_auth = TRUE)
-# eurac %>% listProcesses()
+eurac %>% listProcesses()
 eurac %>% listCapabilities()
-str(eurac %>% listFormats())
+eurac %>% listFormats()
 
-eurac %>% listCollections()
+descriptions = eurac %>% listCollections()
+
 eurac %>% describeCollection("S2_L2A_T32TPS_20M")
 
-eurac %>% describeCollection("S2_L2A_T32TPS_20M")
 eurac %>% describeProcess("filter_bbox")
 eurac %>% describeProcess("filter_daterange")
 eurac %>% describeProcess("NDVI")
 eurac %>% describeProcess("min_time")
 
-task = collection("S2_L2A_T32TPS_20M") %>% 
-  process("filter_bbox", prior.name = "imagery", left=652000,right=672000,bottom=5181000,top=5161000,srs="EPSG:32632") %>% 
-  process("filter_daterange", prior.name="imagery", from= "2017-01-01", to="2017-01-31" ) %>% 
-  process("NDVI", prior.name = "imagery", red="B04",nir="B8A") %>% 
-  process("min_time",prior.name="imagery")
-
 # Build a process graph using the ProcesGraphBuilder pgb()
 pgb = eurac %>% pgb()
 
-alternative = collection("S2_L2A_T32TPS_20M") %>%
-  pgb$filter_bbox(left = 652000, right = 672000, bottom = 5181000, top=5161000,srs="EPSG:32632") %>%
-  pgb$filter_daterange(from = "2017-01-01",to = "2017-01-31") %>%
-  pgb$NDVI(red = "B04", nir = "B8A") %>% 
+task = pgb$collection$S2_L2A_T32TPS_20M %>%
+  pgb$filter_bbox(extent=list(west=10.5,east=11.0,south=46.0,north=46.5)) %>%
+  pgb$filter_daterange(extent = c("2017-01-01T00:00:00Z","2017-01-31T00:00:00Z")) %>%
+  pgb$NDVI(red="B04",nir="B8A") %>%
   pgb$min_time()
 
-taskToJSON(task)
-taskToJSON(alternative)
-
-raster = eurac %>% executeTask(task=task,
+raster = eurac %>% preview(task=task,
                                format="tiff",
                                output_file = "eurac_test.tif")
+
+# eurac %>% listJobs()
+
 job_id = eurac %>% defineJob(task=task,format="tiff")
-str(eurac %>% describeJob(job_id))
 
-list = eurac %>% downloadJob(job_id = job_id,format="tiff")
+eurac %>% describeJob(job_id)
 
-writeBin(list,"test2.tif")
-str(eurac %>% describeJob(job_id))
+eurac %>% orderResults(job_id = job_id)
+
+eurac %>% listResults(job_id)
