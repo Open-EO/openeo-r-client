051b636f8f018e1c89e72d608e08e9b7de93e0f7
Florian Lahn <florian.lahn@uni-muenster.de>
2019-07-18 14:38:56 +0200

    removed a function that was no longer used in 0.4.x

--- R/udf.R
+++ R/udf.R
@@ -14,24 +14,3 @@ list_udf_runtimes = function(con) {
         return(con$request(tag = tag, authorized = FALSE))
     }, error = .capturedErrorToMessage)
 }
-
-#' Gets detailed information about a particular udf type
-#' 
-#' Queries the back-end for a particular runtime and time to retrieve information how the udf_type will work.
-#' 
-#' @param con connected and authenticated openeo client object
-#' @param language the udf runtime identifier
-#' @param type the udf type
-#' @return list with udf runtime type information
-#' @export
-describe_udf_type = function(con, language, type) {
-    tryCatch({
-        if (is.null(language) || is.null(type)) {
-            stop("Missing parameter language or udf_type")
-        }
-        
-        tag = "udf_functions"
-        msg = con$request(tag = tag, parameters = list(language, type), authorized = FALSE)
-        return(msg)
-    }, error = .capturedErrorToMessage)
-}
