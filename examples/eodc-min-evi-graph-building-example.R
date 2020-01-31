47ebab0bdd05e4790069c53ca7c7657692a30e1e
Florian Lahn <florian.lahn@uni-muenster.de>
2020-01-31 10:16:09 +0100

    udated min evi creation script of EODC

--- examples/eodc-min-evi-graph-building-example.R
+++ examples/eodc-min-evi-graph-building-example.R
@@ -1,4 +1,4 @@
-# Example RClient <-> EODC
+# Example RClient (v0.6.0) <-> EODC (0.4.0)
 # https://openeo.org/openeo/news/2019/03/07/openeo-api-040.html
 
 library(openeo)
@@ -6,9 +6,9 @@ library(magrittr)
 library(tibble)
 library(jsonlite)
 
-host_url = "https://openeo.eodc.edu"
+host_url = "https://openeo.eodc.eu"
 
-con = connect(host = host_url, login_type = "oidc",external="google",exchange_token = "id_token")
+con = connect(host = host_url, version="0.4.0", login_type = "oidc",external="google",exchange_token = "id_token")
 
 graph = con %>% process_graph_builder()
 
@@ -20,31 +20,21 @@ data = graph$load_collection(id = graph$data$s2a_prd_msil1c,
                                                    north = 46.5182), 
                              temporal_extent = c("2018-06-04T00:00:00Z","2018-06-23T00:00:00Z"))
 
-spectral_reduce = graph$reduce(data=data, dimension = "bands")
+spectral_reduce = graph$reduce(data=data, dimension = "bands", reducer = function(x) {
+  B08 = x[1]
+  B02 = x[2]
+  B04 = x[3]
+  (2.5 * (B08 - B04)) / sum(B08, 6 * B04, -7.5 * B02,1)
+})
 
-evi_graph = con %>% callback(spectral_reduce,parameter = "reducer", choice_index = 1)
 
-nir = evi_graph$data$data %>% evi_graph$array_element(0)
-red = evi_graph$data$data %>% evi_graph$array_element(1)
-blue = evi_graph$data$data %>% evi_graph$array_element(2)
+temporal_reduce = graph$reduce(data=spectral_reduce, dimension = "temporal", reducer=function(x) {
+  min(x)
+})
 
-p1 = evi_graph$product(data = list(red, 6))
-p2 = evi_graph$product(data = list(blue, -7.5))
+final = graph$save_result(data = temporal_reduce, format = "GTiff")
 
-sub = evi_graph$subtract(data = list(nir,red))
-sum = evi_graph$sum(data = list(1,nir,p1,p2))
-div = evi_graph$divide(data = list(sub,sum))
-
-p3 = evi_graph$product(data = list(2.5,div))
-
-evi_graph$setFinalNode(p3)
-
-temporal_reduce = graph$reduce(data=spectral_reduce, dimension = "temporal")
-
-min_time_graph = con %>% callback(temporal_reduce, parameter = "reducer", choice_index = 1)
-min_time_graph$min(data = min_time_graph$data$data) %>% min_time_graph$setFinalNode()
-
-temporal_reduce %>% graph$save_result(format="GTiff") %>% graph$setFinalNode()
+graph$setFinalNode(node = final)
 
 # print as JSON
 graph
@@ -53,4 +43,3 @@ graph
 graph$validate()
 
 con %>% validate_process_graph(graph)
-
