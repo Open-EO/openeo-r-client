88b29f6beb7e1fe10acc925ecde1b40a2290137c
Florian Lahn <florian.lahn@uni-muenster.de>
2020-07-30 15:36:49 +0200

    further renamings
    - get_user_processes -> describe_user_process
    - delete_process_graph -> delete_user_process
    - create_process_graph -> create_user_process
    - update_process_graph -> update_user_process
    - adaptions in the RD descriptions

--- R/user_defined_processes.R
+++ R/user_defined_processes.R
@@ -6,7 +6,7 @@
 #' 
 #' @param con connected and authenticated openeo client object (optional) otherwise \code{\link{active_connection}}
 #' is used.
-#' @return a list userdefined processes (\code{ProcessInfo})
+#' @return a named list of user defined processes (\code{ProcessInfo})
 #' @export
 list_user_processes = function(con=NULL) {
   tryCatch({
@@ -34,11 +34,11 @@ list_user_processes = function(con=NULL) {
 #' 
 #' @param con connected and authenticated openeo client object (optional) otherwise \code{\link{active_connection}}
 #' is used.
-#' @param id The id of a process graph on the back-end
+#' @param id The id of the user process at the back-end
 #' 
-#' @return the process graph as list
+#' @return the user process as a ProcessInfo class (list object)
 #' @export
-get_user_process = function(id, con=NULL) {
+describe_user_process = function(id, con=NULL) {
   tryCatch({
     con = .assure_connection(con)
     
@@ -59,17 +59,17 @@ get_user_process = function(id, con=NULL) {
   }, error = .capturedErrorToMessage)
 }
 
-#' Deletes a previously stored process graph
+#' Deletes a user process
 #' 
-#' The function initiates the deletion of a process graph on the back-end. Only the owning user can delete
-#' a graph. The graph also should not be part of any particular job.
+#' The function initiates the deletion of a user defined process at the back-end. Only the owning user can delete
+#' their process. The user defined process also should not be part of any particular job.
 #' 
 #' @param con connected and authorized openeo client object (optional) otherwise \code{\link{active_connection}}
 #' is used.
-#' @param id the id of the graph
+#' @param id the id of the user process
 #' 
 #' @export
-delete_process_graph = function(id, con=NULL) {
+delete_user_process = function(id, con=NULL) {
   tryCatch({
     tag = "graph_delete"
     
@@ -83,21 +83,21 @@ delete_process_graph = function(id, con=NULL) {
   }, error = .capturedErrorToMessage)
 }
 
-#' Stores a graph on the back-end
+#' Stores a graph as user defined process at the back-end
 #' 
-#' Uploads the process graph information to the back-end and stores it for reuse.
+#' Uploads the process graph information to the back-end and stores it for reuse as a user defined process.
 #' 
 #' @param con connected and authorized openeo client object (optional) otherwise \code{\link{active_connection}}
 #' is used.
 #' @param graph a process graph definition
-#' @param id the title of the process graph
-#' @param summary the summary of the process graph (optional)
-#' @param description the description of a process graph (optional)
-#' @param submit whether to create a process_graph at the service or to create it for local use
+#' @param id the title of the user process
+#' @param summary the summary for the user process (optional)
+#' @param description the description for the user process (optional)
+#' @param submit whether to create a new user process at the openEO service or to create it for local use (default set to submit = TRUE)
 #' 
 #' @return a list assembling a process graph description or the graph id if send
 #' @export
-create_process_graph = function(graph, id, summary=NULL, description = NULL, submit=TRUE, con=NULL) {
+create_user_process = function(graph, id, summary=NULL, description = NULL, submit=TRUE, con=NULL) {
   tryCatch({
     con = .assure_connection(con)
     
@@ -146,18 +146,16 @@ create_process_graph = function(graph, id, summary=NULL, description = NULL, sub
 #' @param summary summary of the process graph (optional)
 #' @param description description of the process graph (optional)
 #' @export
-update_process_graph = function(id, graph = NULL, summary = NULL, description = NULL, con=NULL) {
+update_user_process = function(id, graph = NULL, summary = NULL, description = NULL, con=NULL) {
   tryCatch({
     if (is.null(id)) {
-      stop("Cannot replace unknown graph. If you want to store the graph, use 'create_process_graph' instead")
+      stop("Cannot replace unknown graph. If you want to store the graph / user defined process, use 'create_user_process' instead")
     }
     
     con = .assure_connection(con)
     
-    #TODO get process_graph via id, then substitue updated stuff and PUT back
     graph_info = get_user_process(con = con, id = id)
     process = processFromJson(json=graph_info)
-    # = list()
     
     if (!is.null(graph)) {
       
@@ -167,51 +165,10 @@ update_process_graph = function(id, graph = NULL, summary = NULL, description = 
       } else {
         process$setProcessGraph(process_graph = graph)
       }
-      
-      
-      #     if (is.function(graph)) {
-      #     graph = as(graph,"Graph")
-      # } else if ("ProcessNode" %in% class(graph)){
-      #     # final node!
-      #     graph = Graph$new(con=con,final_node = graph)
-      # } else if ("Graph" %in% class(graph)) {
-      #     
-      # } else {
-      #     stop("Graph is neither final node, graph nor function.")
-      # }
-      # 
-      # # set or update variables and return
-      # requestBody[["process_graph"]] = graph$serialize()
-      # 
-      # if (length(graph$getVariables()) == 0) {
-      #     graph_params = list()
-      # } else {
-      #     graph_params = lapply(graph$getVariables(),function(p) {
-      #         p$asParameterInfo()
-      #     })
-      # }
-      # requestBody[["parameters"]] = graph_params
-      # 
-      # requestBody[["returns"]] = graph$getFinalNode()$getReturns()$asParameterInfo()
     }
     
     process$setSummary(summary)
     process$setDescription(description)
-    
-    # if (!is.null(summary)) {
-    #     if (is.na(summary)) {
-    #         requestBody[["summary"]] = NULL
-    #     } else {
-    #         requestBody[["summary"]] = summary
-    #     }
-    # }
-    # if (!is.null(description)) {
-    #     if (is.na(description)) {
-    #         requestBody[["description"]] = NULL
-    #     } else {
-    #         requestBody[["description"]] = description
-    #     }
-    # }
     
     requestBody = process$serialize()
     
@@ -226,16 +183,17 @@ update_process_graph = function(id, graph = NULL, summary = NULL, description = 
   }, error = .capturedErrorToMessage)
 }
 
-#' Validates a process graph
+#' Validates a user process
 #' 
-#' Sends the process graph to the back-end and validates it against the offered processes by the backend.
+#' Sends the process graph as a user process to the openEO service and validates it against the predefined and user-defined 
+#' processes of the service.
 #' 
 #' @param con connected and authorized openeo client object (optional) otherwise \code{\link{active_connection}}
 #' is used.
-#' @param graph the process graph that will be sent to the back-end and is being validated
+#' @param graph the process graph that will be sent to the service and is going to be validated
 #' 
 #' @export
-validate_process_graph = function(graph, con=NULL) {
+validate_process = function(graph, con=NULL) {
   tryCatch({
     con = .assure_connection(con)
     
@@ -272,7 +230,7 @@ validate_process_graph = function(graph, con=NULL) {
 
 #' Process collection for user defined processes
 #' 
-#' The created process graphs via \code{\link{create_process_graph}} at the openEO service are user defined processes. 
+#' The created process graphs via \code{\link{create_user_process}} at the openEO service are user defined processes. 
 #' This means that they can be used within the creation of process graphs themselves. For processes provided by the 
 #' particular openEO service the \code{\link{processes}} function can be used to obtain a builder for those processes. 
 #' Analoguous to this idea, this function creates a builder object for user defined proceses which are listed and described
