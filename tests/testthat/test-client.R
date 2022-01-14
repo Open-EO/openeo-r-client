1738db7bace092fc5ec6d6576be9b2778c411c22
florian.lahn <florian.lahn@eftas.com>
2022-01-14 14:00:58 +0100

    added connect test for a valid url (#100)

--- tests/testthat/test-client.R
+++ tests/testthat/test-client.R
@@ -4,3 +4,11 @@ test_that("connect with invalid url fails when setting via initialization", {
   
   expect_error(x$connect(),regexp = "resolve host: example.foo")
 })
+
+test_that("connect with valid url", {
+  skip_if_offline()
+  x = openeo:::OpenEOClient$new(host = "https://openeo.cloud")
+  
+  con = x$connect()
+  expect(TRUE,failure_message = "connecting got an error")
+})
