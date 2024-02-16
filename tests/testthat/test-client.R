c23a50c3ab102b674507edf8671c3e2be99b3264
florian.lahn <florian.lahn@eftas.com>
2024-02-16 12:00:22 +0100

    removed exact error message in connect test

--- tests/testthat/test-client.R
+++ tests/testthat/test-client.R
@@ -1,8 +1,7 @@
 test_that("connect with invalid url fails when setting via initialization", {
   x = openeo:::OpenEOClient$new(host = "example.foo")
   
-  
-  expect_error(x$connect(),regexp = "resolve host: example.foo")
+  expect_error(x$connect())
 })
 
 test_that("connect with missing url fails when setting via initialization", {
